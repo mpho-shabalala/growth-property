2de34937d608328967485d3c7172dd3bdc5a605b
mpho-shabalala <mphog3401@gmail.com>
2025-10-15 14:31:30 +0100

    predeploy error fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './hero.css'
 
 function Hero() {
--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { Link } from 'react-router-dom'
 import type { PropertyType } from '../types'
 import './Property.css';
